163dbcffad534445b39a941250e571b5fc8f1974
ashaduzzaman <ashaduzzaman.nstu1994@gmail.com>
2026-04-18 15:53:13 -0400

    Fresh start

--- src/tools/index.ts
+++
@@ -1,6 +0,0 @@
-export { getClientContext } from "./get-client-context.js";
-export { logDecision } from "./log-decision.js";
-export { getDecisionTrail } from "./get-decision-trail.js";
-export { flagRisk } from "./flag-risk.js";
-export { summarizeProjectWeek } from "./summarize-project-week.js";
-export { detectScopeCreep } from "./detect-scope-creep.js";
